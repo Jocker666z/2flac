f4b854d5498c95e83acd42218684f09072371429
Jocker666z <r.barbarot@gmail.com>
2024-02-03 17:24:59 +0100

    Minor update

--- 2flac.sh
+++ 2flac.sh
@@ -477,7 +477,7 @@ for file in "${lst_audio_flac_compressed[@]}"; do
 			done
 		done
 
-		# Remove duplicate tags_
+		# Remove duplicate tags
 		mapfile -t source_tag < <( printf '%s\n' "${source_tag[@]}" | uniq -u )
 
 		# Tag FLAC
